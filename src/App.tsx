08fe5a976536985f5798c237e0d3550c90d30c2c
Abdul Samad Mahmood <72197815+ASMahmood@users.noreply.github.com>
2021-03-14 20:00:44 +0000

    creating homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,16 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Route, withRouter } from "react-router-dom";
 
 import NavBar from "./components/NavBar";
+import HomePage from "./pages/Home/HomePage";
 
 function App() {
   return (
-    <div className="App">
+    <div id="globalStyle">
       <NavBar />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Route path="/" exact component={HomePage} />
     </div>
   );
 }
---
+++ src/pages/Home/HomePage.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import "./HomePage.css";
+
+function HomePage() {
+  return (
+    <Container className="homeBody">
+      <Row>
+        <Col xs={12}> HI</Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default HomePage;
