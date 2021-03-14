4e4bcff399ba36a8a48bcd03e11bbad0e81e949c
Abdul Samad Mahmood <72197815+ASMahmood@users.noreply.github.com>
2021-03-14 20:43:14 +0000

    created structure placeholders for homepage

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -6,7 +6,19 @@ function HomePage() {
   return (
     <Container className="homeBody">
       <Row>
-        <Col xs={12}> HI</Col>
+        <Col xs={12} className="userBox"></Col>
+      </Row>
+      <Row className="mt-4">
+        <Col xs={3} className="roomList"></Col>
+        <Col xs={9} className="pl-5">
+          <Row>
+            <Col xs={12} className="createRoom"></Col>
+          </Row>
+          <Row className="mt-4">
+            <Col xs={6} className="friendList"></Col>
+            <Col xs={{ span: 5, offset: 1 }} className="randoMeme"></Col>
+          </Row>
+        </Col>
       </Row>
     </Container>
   );
