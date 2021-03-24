688604ea2225c98515656693e1eec63c47ffff2e
Abdul Samad Mahmood <72197815+ASMahmood@users.noreply.github.com>
2021-03-24 08:09:47 +0000

    yet more styling

--- src/components/WhiteBoardOptions.tsx
+++ src/components/WhiteBoardOptions.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Form } from "react-bootstrap";
 import { connect } from "react-redux";
 import { reduxStore } from "../types/reduxInterface";
+import { FaPalette } from "react-icons/fa";
 import "./styles/WhiteBoard.css";
 
 const mapStateToProps = (state: reduxStore) => state;
@@ -9,7 +10,6 @@ const mapStateToProps = (state: reduxStore) => state;
 function WhiteBoardOptions() {
   return (
     <div id="boardOptions" className="d-flex px-3 align-items-center">
-      <Form.Control type="color" id="colorPicker" />
       <Form.Control
         type="number"
         placeholder="2"
@@ -18,6 +18,10 @@ function WhiteBoardOptions() {
         className="ml-2"
       />
       px width
+      <Form.Label className="ml-4" htmlFor="colorPicker">
+        <FaPalette size="large" />
+      </Form.Label>
+      <Form.Control type="color" id="colorPicker" />
     </div>
   );
 }
