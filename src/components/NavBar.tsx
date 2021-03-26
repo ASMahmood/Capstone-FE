81a88a6c943bec00161eb4eeb6b97c17cc1fa0d3
Abdul Samad Mahmood <72197815+ASMahmood@users.noreply.github.com>
2021-03-26 07:48:07 +0000

    some css stuff, mainly navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import { Nav, Navbar, Form, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import {
+  AiOutlineSearch,
+  AiOutlineHome,
+  AiOutlineLogout,
+} from "react-icons/ai";
+import "./styles/NavBar.css";
 
 function NavBar() {
   return (
-    <Navbar bg="dark" variant="dark" expand="lg">
+    <Navbar>
       <Link to="/">
         <Navbar.Brand>#NEED_A_NAME</Navbar.Brand>
       </Link>
@@ -12,15 +18,28 @@ function NavBar() {
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
           <Link to="/">
-            <span className="nav-link">Home</span>
+            <div className="navDiv d-flex flex-column align-items-center justify-content-center ">
+              <AiOutlineHome fontSize="18" />
+              <span className="nav-link py-0">Home</span>
+            </div>
           </Link>
           <Link to="/login">
-            <span className="nav-link">Login</span>
+            <div className="navDiv d-flex flex-column align-items-center justify-content-center ">
+              <AiOutlineLogout fontSize="18" />
+              <span className="nav-link py-0">Logout</span>
+            </div>
           </Link>
         </Nav>
         <Form inline>
-          <Form.Control type="text" placeholder="Search" className="mr-sm-2" />
-          <Button variant="outline-warning">Search</Button>
+          <Form.Control
+            type="text"
+            id="navbarSearch"
+            placeholder="Search"
+            className="mr-sm-2"
+          />
+          <Button variant="secondary">
+            <AiOutlineSearch fontSize="22" />
+          </Button>
         </Form>
       </Navbar.Collapse>
     </Navbar>
