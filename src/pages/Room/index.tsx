a485ba78d85c8d6885db649db91f946d8410ac71
Abdul Samad Mahmood <72197815+ASMahmood@users.noreply.github.com>
2021-03-29 07:35:36 +0100

    Update index.tsx

--- src/pages/Room/index.tsx
+++ src/pages/Room/index.tsx
@@ -1,15 +1,19 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./style.css";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Button } from "react-bootstrap";
 import { AiOutlineUserAdd } from "react-icons/ai";
 import { connect } from "react-redux";
 import { RouteComponentProps, withRouter } from "react-router-dom";
 import { Dispatch } from "redux";
 import { reduxStore } from "../../types/reduxInterface";
-import { populateRoomDispatch } from "../../types/dispatchInterfaces";
+import {
+  populateRoomDispatch,
+  populateUserDispatch,
+} from "../../types/dispatchInterfaces";
 import { MatchParams } from "../../types/routerDomInterfaces";
 import { leaveRoom } from "../../functions/socket";
-import { fetchRoom } from "../../functions/api";
+import { fetchUserInfo } from "../../functions/other";
+import { fetchRoom, addUserToRoom, fetchMe } from "../../functions/api";
 import WhiteBoard from "../../components/WhiteBoard";
 import WhiteBoardOption from "../../components/WhiteBoardOptions";
 import ChatList from "../../components/ChatList";
@@ -18,6 +22,7 @@ import InviteUsers from "../../components/InviteUsers";
 
 type roomProps = reduxStore &
   populateRoomDispatch &
+  populateUserDispatch &
   RouteComponentProps<MatchParams>;
 
 const mapStateToProps = (state: reduxStore) => state;
@@ -28,12 +33,31 @@ const mapDispatchToProps = (dispatch: Dispatch) => ({
       type: "POPULATE_ROOM",
       payload: room,
     }),
+  populateUser: (user: object) =>
+    dispatch({
+      type: "POPULATE_USER",
+      payload: user,
+    }),
 });
 
 function RoomPage(props: roomProps) {
+  const [joining, setJoin] = useState<boolean>(false);
+
   useEffect(() => {
+    const checkIfOnline = async () => {
+      const response = await fetchUserInfo();
+      if (response) {
+        const user = await fetchMe();
+        props.populateUser(user);
+      } else {
+        props.history.push("/login");
+      }
+    };
+    checkIfOnline();
     let searchParams = new URLSearchParams(props.location.search);
-    console.log(searchParams.get("join"));
+    if (searchParams.get("join")) {
+      setJoin(true);
+    }
     const getAndPopulate = async (id: string) => {
       const response = await fetchRoom(id);
       if (Object.keys(response).length > 1) {
@@ -43,6 +67,7 @@ function RoomPage(props: roomProps) {
       }
     };
     getAndPopulate(props.match.params.id);
+
     return () => {
       leaveRoom({
         roomId: props.room._id,
@@ -51,24 +76,56 @@ function RoomPage(props: roomProps) {
       });
     };
   }, []);
+
+  const handleJoin = async (e: React.MouseEvent) => {
+    e.preventDefault();
+    let res = await addUserToRoom(props.room._id, props.user._id);
+    if (res.message === "authorized") {
+      setJoin(false);
+    }
+  };
   return (
     <Container fluid className="roomBody">
       <Row>
         <Col xs={12} className="roomNavTop d-flex align-items-center">
           <h2 className="m-0">{props.room.name} </h2>
           <h5 className="ml-3"> {props.room.participants.length} members</h5>
-          <InviteUsers />
+          {joining ? (
+            <Button
+              className="ml-auto"
+              onClick={(e) => handleJoin(e)}
+              variant="secondary"
+            >
+              JOIN
+            </Button>
+          ) : (
+            <InviteUsers />
+          )}
         </Col>
       </Row>
       <Row className="mt-4">
-        <Col xs={9} className="whiteBoard">
-          <WhiteBoard />
-          <WhiteBoardOption />
-        </Col>
-        <Col xs={3} className="chatBox d-flex flex-column justify-content-end">
-          <ChatList />
-          <SendMessage />
-        </Col>
+        {joining ? (
+          <Col
+            xs={12}
+            className="whiteBoard p-3 d-flex justify-content-center align-items-center text-center"
+          >
+            <h1>Please join this room to see its contents</h1>
+          </Col>
+        ) : (
+          <>
+            <Col xs={9} className="whiteBoard">
+              <WhiteBoard />
+              <WhiteBoardOption />
+            </Col>
+            <Col
+              xs={3}
+              className="chatBox d-flex flex-column justify-content-end"
+            >
+              <ChatList />
+              <SendMessage />
+            </Col>
+          </>
+        )}
       </Row>
     </Container>
   );
