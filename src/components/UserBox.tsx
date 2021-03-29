71d5d04ec63edcfa75d49a3d95252e0153388e79
Abdul Samad Mahmood <72197815+ASMahmood@users.noreply.github.com>
2021-03-29 13:54:10 +0100

    stuff

--- src/components/UserBox.tsx
+++ src/components/UserBox.tsx
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 import { connect } from "react-redux";
 import { reduxStore } from "../types/reduxInterface";
+import "./styles/UserBox.css";
 
 const mapStateToProps = (state: reduxStore) => state;
 
 function UserBox(props: reduxStore) {
-  return <div>Hello there, {props.user.username}</div>;
+  return (
+    <div className="userInfo">
+      <Form>
+        <Form.Group>
+          <Form.Label htmlFor="ProfilePicSelector">
+            <img src={props.user.profilePic} alt="profile" />
+          </Form.Label>
+          <Form.Control
+            type="file"
+            accept="image/*"
+            id="ProfilePicSelector"
+            className="filePickerInput"
+          />
+        </Form.Group>
+      </Form>
+      <h3>Hello there, {props.user.username}</h3>
+    </div>
+  );
 }
 
 export default connect(mapStateToProps)(UserBox);
